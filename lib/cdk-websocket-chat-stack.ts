2b7428f61016c2e01726d6ee5beb6df77c27e786
tetsuya.zama <tetsuya.zama@gmail.com>
2021-04-04 06:05:34 +0900

    add cdk stack

--- lib/cdk-websocket-chat-stack.ts
+++ lib/cdk-websocket-chat-stack.ts
@@ -1,9 +1,101 @@
 import * as cdk from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as lmdjs from '@aws-cdk/aws-lambda-nodejs';
+import * as apigw from '@aws-cdk/aws-apigatewayv2';
+import * as iam from '@aws-cdk/aws-iam';
+import {LambdaWebSocketIntegration} from '@aws-cdk/aws-apigatewayv2-integrations';
 
 export class CdkWebsocketChatStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const connectionTable = new dynamodb.Table(this, 'connection-table', {
+      partitionKey: {name: "h_key", type: dynamodb.AttributeType.STRING},
+      sortKey: {name: "s_key", type: dynamodb.AttributeType.STRING},
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+
+    const useridIdxName = "user_id_idx";
+    connectionTable.addGlobalSecondaryIndex({
+      indexName: useridIdxName,
+      partitionKey: {name: "user_id", type: dynamodb.AttributeType.STRING}
+    });
+
+    const websocketApi = new apigw.WebSocketApi(this, 'chat-ws-api', {routeSelectionExpression: '$request.body.action'});
+    const websocketPostPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [`arn:aws:execute-api:${this.region}:${this.account}:${websocketApi.apiId}/*/POST/@connections/*`],
+      actions: ["execute-api:ManageConnections"]
+    });
+
+    const lmdEnvironments = {
+      CONNECTION_TABLE_NAME: connectionTable.tableName,
+      USERID_INDEX_NAME: useridIdxName,
+      WEBSOCKET_ENDPOINT: websocketApi.apiEndpoint
+    }
+
+    const onConnect = new lmdjs.NodejsFunction(this, 'on-connect', {
+      entry: "./lib/handlers.ts",
+      handler: "onConnectHandler",
+      environment: lmdEnvironments
+    });
+
+    onConnect.addToRolePolicy(websocketPostPolicy);
+    connectionTable.grantReadWriteData(onConnect);
+
+    const onDisconnect = new lmdjs.NodejsFunction(this, 'on-disconnect', {
+      entry: "lib/handlers.ts",
+      handler: "onDisconnectHandler",
+      environment: lmdEnvironments
+    });
+
+    onDisconnect.addToRolePolicy(websocketPostPolicy);
+    connectionTable.grantReadWriteData(onDisconnect);
+
+    const onRoomMessage = new lmdjs.NodejsFunction(this, 'on-room-message', {
+      entry: "lib/handlers.ts",
+      handler: "roomMessageHandler",
+      environment: lmdEnvironments
+    });
+
+    onRoomMessage.addToRolePolicy(websocketPostPolicy);
+    connectionTable.grantReadData(onRoomMessage);
+
+    const onDirectMessage = new lmdjs.NodejsFunction(this, 'on-direct-message', {
+      entry: "lib/handlers.ts",
+      handler: "directMessageHandler",
+      environment: lmdEnvironments
+    });
+
+    onDirectMessage.addToRolePolicy(websocketPostPolicy);
+    connectionTable.grantReadData(onDirectMessage);
+
+    const onUserlistRequest = new lmdjs.NodejsFunction(this, 'on-userlist-request', {
+      entry: "lib/handlers.ts",
+      handler: "userListRequestHandler",
+      environment: lmdEnvironments
+    });
+
+    onUserlistRequest.addToRolePolicy(websocketPostPolicy);
+    connectionTable.grantReadData(onUserlistRequest);
+
+    websocketApi.addRoute('$connect', {
+      integration: new LambdaWebSocketIntegration({handler: onConnect})
+    });
+    websocketApi.addRoute('$disconnect', {
+      integration: new LambdaWebSocketIntegration({handler: onConnect})
+    });
+    websocketApi.addRoute('$default', {
+      integration: new LambdaWebSocketIntegration({handler: onUserlistRequest})
+    });
+    websocketApi.addRoute('roommessage', {
+      integration: new LambdaWebSocketIntegration({handler: onRoomMessage})
+    });
+    websocketApi.addRoute('directmessage', {
+      integration: new LambdaWebSocketIntegration({handler: onDirectMessage})
+    });
+    websocketApi.addRoute('userlistrequest', {
+      integration: new LambdaWebSocketIntegration({handler: onUserlistRequest})
+    });
   }
 }
--- test/cdk-websocket-chat.test.ts
+++ test/cdk-websocket-chat.test.ts
@@ -1,13 +1,83 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
 import * as CdkWebsocketChat from '../lib/cdk-websocket-chat-stack';
 
-test('Empty Stack', () => {
+test('Websocket chat Stack', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkWebsocketChat.CdkWebsocketChatStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(stack).toHaveResourceLike('AWS::DynamoDB::Table', {
+      "KeySchema": [
+        {
+          "AttributeName": "h_key",
+          "KeyType": "HASH"
+        },
+        {
+          "AttributeName": "s_key",
+          "KeyType": "RANGE"
+        }
+      ],
+      "AttributeDefinitions": [
+        {
+          "AttributeName": "h_key",
+          "AttributeType": "S"
+        },
+        {
+          "AttributeName": "s_key",
+          "AttributeType": "S"
+        },
+        {
+          "AttributeName": "user_id",
+          "AttributeType": "S"
+        }
+      ],
+      "BillingMode": "PAY_PER_REQUEST",
+      "GlobalSecondaryIndexes": [
+        {
+          "IndexName": "user_id_idx",
+          "KeySchema": [
+            {
+              "AttributeName": "user_id",
+              "KeyType": "HASH"
+            }
+          ],
+          "Projection": {
+            "ProjectionType": "ALL"
+          }
+        }
+      ]
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Api', {
+      "Name": "chat-ws-api",
+      "ProtocolType": "WEBSOCKET",
+      "RouteSelectionExpression": "$request.body.action"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "$connect"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "$disconnect"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "$default"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "roommessage"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "directmessage"
+    });
+    
+    expect(stack).toHaveResourceLike('AWS::ApiGatewayV2::Route', {
+      "RouteKey": "userlistrequest"
+    });
+
+    expect(stack).toHaveResourceLike('AWS::Lambda::Function')
 });
