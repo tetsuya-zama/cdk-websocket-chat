1680633dc207f7bdab27b538f797c944a3bddc47
tetsuya.zama <tetsuya.zama@gmail.com>
2021-04-04 09:23:49 +0900

    add pipeline

--- bin/cdk-websocket-chat.ts
+++ bin/cdk-websocket-chat.ts
@@ -1,21 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkWebsocketChatStack } from '../lib/cdk-websocket-chat-stack';
+import { CdkWebsocketChatPipeline } from '../lib/cdk-websocket-chat-pipeline';
 
 const app = new cdk.App();
-new CdkWebsocketChatStack(app, 'CdkWebsocketChatStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new CdkWebsocketChatPipeline(app, 'CdkWebsocketChatPipeline');
---
+++ lib/cdk-websocket-chat-pipeline.ts
@@ -0,0 +1,54 @@
+import {CdkWebsocketChatStack} from './cdk-websocket-chat-stack';
+
+import {Construct, SecretValue, Stack, StackProps, Stage, StageProps} from '@aws-cdk/core';
+import {Artifact} from '@aws-cdk/aws-codepipeline';
+import {GitHubSourceAction} from '@aws-cdk/aws-codepipeline-actions';
+import {CdkPipeline, SimpleSynthAction} from '@aws-cdk/pipelines';
+
+class WebsocketChatApplication extends Stage{
+    constructor(scope: Construct, id: string, props?: StageProps){
+        super(scope, id, props);
+
+        new CdkWebsocketChatStack(this, 'webchatapp');
+    }
+}
+
+export class CdkWebsocketChatPipeline extends Stack {
+    constructor(scope: Construct, id:string, props?: StackProps){
+        super(scope, id, props);
+
+        const sourceArtifact = new Artifact();
+        const cloudAssemblyArtifact = new Artifact();
+
+        const pipeline = new CdkPipeline(this, 'webchat-app-pipeline', {
+            pipelineName: "webchat-app-pipeline",
+            cloudAssemblyArtifact,
+
+            sourceAction: new GitHubSourceAction({
+                actionName: 'GitHub',
+                output: sourceArtifact,
+                oauthToken: SecretValue.secretsManager("github_token", {jsonField: "GITHUB_TOKEN"}),
+                owner: 'tetsuya-zama',
+                repo: 'cdk-websocket-chat'
+            }),
+
+            synthAction: SimpleSynthAction.standardYarnSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+                environment: {
+                    privileged: true
+                },
+                synthCommand: 'yarn cdk synth'
+            })
+        });
+
+        pipeline.addApplicationStage(
+            new WebsocketChatApplication(this, 'staging')
+        );
+
+        pipeline.addApplicationStage(
+            new WebsocketChatApplication(this, 'prod'),
+            {manualApprovals: true}
+        );
+    }
+}
